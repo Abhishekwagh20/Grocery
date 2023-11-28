7897e656ede5aa5ad37dfbc09f13e47bb70298d3
Abhishek Wagh <abhimwagh20@gmail.com>
2023-11-27 21:13:42 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ import Menu from "./page/Menu";
 import About from "./page/About";
 import Contact from "./page/Contact";
 import Login from "./page/login";
+import Newproduct from "./page/Newproduct";
+import Signup from "./page/Signup";
+import { store } from "./redux/index";
+import { Provider } from "react-redux";
 
 
 const router = createBrowserRouter(
@@ -25,15 +29,21 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
       <Route path="login" element={<Login />} />
+      <Route path="newproduct" element={<Newproduct />} />
+      <Route path="signup" element={<Signup />} />
+
+
     </Route>
   )
 );
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider router={router} />
-  );
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
