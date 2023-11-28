4386d84899025b8b0b106df9210f1031b09c34cc
Abhishek Wagh <abhimwagh20@gmail.com>
2023-11-27 21:13:37 -0600

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
-import logo from "../assest/logo.png"
+import logo from "../assets/logo.png"
 import { Link } from 'react-router-dom'
 import {HiOutlineUserCircle} from "react-icons/hi"
 import {BsCartFill} from "react-icons/bs"
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-hot-toast";
+import { logoutRedux } from "../redux/userSlice";
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const userData = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+
     const handleShowMenu = () => {
         setShowMenu((preve) => !preve);
+    };
+    const handleLogout = () => {
+        dispatch(logoutRedux());
+        toast("Logout successfully");
       };
   return (
     <header className='fixed shadow-md w-full h-16 px-2 md:px-4 z-50'>
@@ -31,12 +41,31 @@ const Header = () => {
                 </div>
                 <div className=' text-slate-600 'onClick={handleShowMenu}>
                     <div className='text-3xl cursor-pointer w-8 h-8 rounded-full overflow-hidden drop-shadow-md' >
-                        <HiOutlineUserCircle/>
+                    {userData.image ? (
+                <img src={userData.image} className="h-full w-full" />
+              ) : (
+                <HiOutlineUserCircle />
+              )}
                     </div>
                     {
                         showMenu && <div className='absolute right-2 bg-white py-2  shadow drop-shadow-md flex flex-col min-w-[120px] text-center'>
-                        <p className="whitespace-nowrap cursor-pointer px-2">New Product</p>
-                        <p className="whitespace-nowrap cursor-pointer px-2">Login</p>
+                        <Link to={"newproduct"} className="whitespace-nowrap cursor-pointer px-2">New Product</Link>
+                        {userData.image ? (
+                  <p
+                    className="cursor-pointer text-white px-2 bg-red-500"
+                    onClick={handleLogout}
+                  >
+                    Logout ({userData.firstName}){" "}
+                  </p>
+                ) : (
+                  <Link
+                    to={"login"}
+                    className="whitespace-nowrap cursor-pointer px-2"
+                  >
+                    Login
+                  </Link>
+                )}
+                        
                             </div>
                     }
                     
