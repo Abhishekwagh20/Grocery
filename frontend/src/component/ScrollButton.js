4299e0d2d65127ed0931923cded05cfea1010005
Abhishek Wagh <abhimwagh20@gmail.com>
2023-12-13 22:48:10 -0600

    Update ScrollButton.js

--- frontend/src/component/ScrollButton.js
+++ frontend/src/component/ScrollButton.js
@@ -5,13 +5,13 @@ const ScrollButton = () => {
     // Scroll to a specific position or element on the page
     window.scrollTo({
       top: 360,
-      behavior: 'smooth', // Optional: Adds smooth scrolling animation
+      behavior: 'smooth', 
     });
   };
 
   return (
     <div>
-      {/* Your other components */}
+      
       <button className="font-bold bg-red-500 text-slate-200 px-4 py-2 rounded-md" onClick={handleScrollDown}>Order now</button>
     </div>
   );
