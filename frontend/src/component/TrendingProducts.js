31bf5f38b4884aefea5e5d6bb6c97dbdd9d2b09f
Abhishek Wagh <abhimwagh20@gmail.com>
2023-12-13 22:35:23 -0600

    Create TrendingProducts.js

---
+++ frontend/src/component/TrendingProducts.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import CardFeature from "./CardFeature";
+import FilterProduct from "./FilterProduct";
+
+const TrendingProducts = ({ heading }) => {
+  const productData = useSelector((state) => state.product.productList);
+  const categoryList = [...new Set(productData.map((el) => el.category))];
+
+  //filter data display
+  const [filterby, setFilterBy] = useState("");
+  const [dataFilter, setDataFilter] = useState([]);
+
+  useEffect(() => {
+    setDataFilter(shuffleArray(productData).slice(0, 5));
+  }, [productData]);
+
+  const handleFilterProduct = (category) => {
+    setFilterBy(category)
+    const filter = productData.filter(
+      (el) => el.category.toLowerCase() === category.toLowerCase()
+    );
+    setDataFilter(() => {
+      return shuffleArray(filter).slice(0, 8);
+    });
+  };
+
+  const loadingArrayFeature = new Array(10).fill(null);
+
+  // Function to shuffle an array
+  const shuffleArray = (array) => {
+    const shuffledArray = array.slice();
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+  };
+
+  return (
+    <div className="my-5">
+      <h2 className="font-bold text-2xl text-slate-800 mb-4">{heading}</h2>
+
+      <div className="flex gap-4 justify-center overflow-scroll scrollbar-none">
+        {categoryList[0] ? (
+          categoryList.map((el) => {
+            return (
+              <FilterProduct
+                category={el}
+                key={el}
+                isActive={el.toLowerCase() === filterby.toLowerCase()}
+                onClick={() => handleFilterProduct(el)}
+              />
+            );
+          })
+        ) : (
+          <div className="min-h-[150px] flex justify-center items-center">
+            <p>Loading...</p>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 my-4">
+        {dataFilter[0]
+          ? dataFilter.map((el) => {
+              return (
+                <CardFeature
+                  key={el._id}
+                  id={el._id}
+                  image={el.image}
+                  name={el.name}
+                  category={el.category}
+                  price={el.price}
+                />
+              );
+            })
+          : 
+          loadingArrayFeature.map((el,index) => (
+              <CardFeature loading="Loading..." key={index+"trendingproducts"} />
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrendingProducts;
